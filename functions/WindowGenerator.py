e0380a5a2a4ceb30df16c3b17c6d1ba5a8150306
afletcher53 <afletcher53@gmail.com>
2022-06-13 23:12:26 +0100

    decoupled text vectorization and window generator

--- functions/WindowGenerator.py
+++ functions/WindowGenerator.py
@@ -1,7 +1,6 @@
 import numpy as np
-import tensorflow as tf
 import tqdm
-from constants import MAX_SEQUENCE_LENGTH
+from constants import OOV_TOKEN
 from functions.clamp import clamp
 
 
@@ -15,21 +14,23 @@ class WindowGenerator:
         self.minimum_day_of_year: int = 0
         self.maximum_day_of_year: int = 365
 
-    def split_window(self, data, tv):
+    def split_window(self, data):
         """Creates split dataset
 
         Args:
             data (pan): _description_
-            tv (_type_): _description_
-
         Returns:
             _type_: _description_
         """
-        all_labels: int = []
+        all_labels: list = []
         all_input_timestep_sequences: list = []
-        non_null_indexes = np.argwhere(
-            data.notnull().values
-        ).tolist()  # Get indexes of df where values which are not null
+        data = data.head(10)
+        data = data.replace(r"^\s*$", np.nan, regex=True)
+        non_null_indexes = list(
+            zip(*np.where(data.notnull()))
+        )  # Get indexes of df where values which are not null
+        newdf = data.notnull().astype("int")
+        print(newdf.head())
         for i in tqdm.tqdm(range(len(data.index)), ncols=100, desc="Windowing data.."):
             input_sequence: list = []  # Input sequence per patient
             visit_indexes = [
@@ -49,6 +50,7 @@ class WindowGenerator:
                 )
                 row_slice = data.iloc[[i]]
                 input_sequence = row_slice.iloc[:, lower_bound + 1 : visit_index + 1]
+                print("")
                 label_sequence = row_slice.iloc[:, visit_index + 1 : upper_bound]
 
                 if label_sequence.isnull().all().all():
@@ -57,29 +59,19 @@ class WindowGenerator:
                     all_labels.append(1)
                 for column in input_sequence:
                     if input_sequence[column].isnull().values.any():
-                        input_timesteps_sequence.append(
-                            tf.zeros([1, MAX_SEQUENCE_LENGTH], tf.int32)
-                        )
+                        input_timesteps_sequence.append(OOV_TOKEN)
                     else:
-                        input_timesteps_sequence.append(tv(input_sequence[column]))
+                        input_timesteps_sequence.append(
+                            ",".join(str(x) for x in input_sequence[column].values)
+                        )
 
                 # check that the input sequence matches the input_width
                 if len(input_timesteps_sequence) != self.input_width:
                     # find difference in length and pad from the left of the input sequence
                     diff = self.input_width - len(input_timesteps_sequence)
                     for _ in range(diff):
-                        input_timesteps_sequence.insert(
-                            0, tf.zeros([1, MAX_SEQUENCE_LENGTH], tf.int32)
-                        )
+                        input_timesteps_sequence.insert(0, OOV_TOKEN)
 
                 all_input_timestep_sequences.append(input_timesteps_sequence)
 
-        # Colapse last 3 dimensions into single sequence
-        all_input_timestep_sequences = np.asarray(all_input_timestep_sequences)
-        all_input_timestep_sequences = all_input_timestep_sequences.reshape(
-            *all_input_timestep_sequences.shape[:-2], -1
-        )
-        all_input_timestep_sequences = tf.cast(
-            all_input_timestep_sequences, dtype="float64"
-        )
-        return np.asarray(all_input_timestep_sequences), np.asarray(all_labels)
+        return all_input_timestep_sequences, all_labels
