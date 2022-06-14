d9b2705410ac68a6fc48bc6fe957d43caeab0d6c
afletcher53 <afletcher53@gmail.com>
2022-06-14 22:40:47 +0100

    remove glove

--- constants.py
+++ constants.py
@@ -1,4 +1,4 @@
-EMBEDDING_DIM: int = 100  # Dimensions of the word vectors
+EMBEDDING_DIM: int = 300  # Dimensions of the word vectors
 MAX_VOCAB_SIZE: int = (
     10000  # how many unique words to use (i.e num rows in embedding vector)
 )
@@ -12,7 +12,12 @@ VALIDATION_SPLIT: float = 0.2
 SEED = 2022
 DATA_FILEPATH = "./data/data.csv"
 REARRANGED_DATA_FILEPATH = "./data/rearranged_data.csv"
+REARRANGED_SINGLE_INPUT_WINDOWED_DATA_FILEPATH = "./data/windowed_single_input.pkl"
+REARRANGED_SINGLE_INPUT_WINDOWED_LABEL_FILEPATH = "./data/windowed_single_labels.pkl"
 MAX_SEQUENCE_LENGTH: int = 200  # max number of words in a comment to use
 MAX_VOCAB_SIZE: int = (
     10000  # how many unique words to use (i.e num rows in embedding vector)
 )
+LR = 1e-3
+
+GLOVE_300D_FILEPATH = "./data/glove.6B.300d.txt"
--- functions/WindowGenerator.py
+++ functions/WindowGenerator.py
@@ -1,37 +1,44 @@
+import time
+from typing import Tuple
 import numpy as np
+import pandas
 import tqdm
-from constants import OOV_TOKEN
+from constants import (
+    OOV_TOKEN,
+    REARRANGED_SINGLE_INPUT_WINDOWED_DATA_FILEPATH,
+    REARRANGED_SINGLE_INPUT_WINDOWED_LABEL_FILEPATH,
+)
 from functions.clamp import clamp
+import pickle
 
 
 class WindowGenerator:
-    """Class to generate timestep data"""
+    """
+    Class to generate timestep'd data
+    """
 
-    def __init__(self, input_width: int, output_width: int):
+    def __init__(self, input_width: int, output_width: int, save_windows: bool):
+        """Init Parmas
+
+        Args:
+            input_width (int): The timesteps forming the input sequence
+            output_width (int): The timesteps forming the output sequence
+        """
         self.input_width: int = input_width
         self.output_width: int = output_width
         self.total_window_size: int = input_width + output_width
         self.minimum_day_of_year: int = 0
         self.maximum_day_of_year: int = 365
+        self.save_windows: bool = save_windows
 
-    def split_window(self, data):
-        """Creates split dataset
+    def split_window(self, data: pandas.DataFrame) -> Tuple[np.ndarray, np.ndarray]:
 
-        Args:
-            data (pan): _description_
-        Returns:
-            _type_: _description_
-        """
         all_labels: list = []
         all_input_timestep_sequences: list = []
-        data = data.head(10)
-        data = data.replace(r"^\s*$", np.nan, regex=True)
         non_null_indexes = list(
             zip(*np.where(data.notnull()))
         )  # Get indexes of df where values which are not null
-        newdf = data.notnull().astype("int")
-        print(newdf.head())
-        for i in tqdm.tqdm(range(len(data.index)), ncols=100, desc="Windowing data.."):
+        for i in tqdm.tqdm(range(len(data.index)), ncols=100, desc="Windowing data..."):
             input_sequence: list = []  # Input sequence per patient
             visit_indexes = [
                 item[1] for item in non_null_indexes if item[0] == i
@@ -53,10 +60,8 @@ class WindowGenerator:
                 print("")
                 label_sequence = row_slice.iloc[:, visit_index + 1 : upper_bound]
 
-                if label_sequence.isnull().all().all():
-                    all_labels.append(0)
-                else:
-                    all_labels.append(1)
+                all_labels.append(self._categorize_output_sequence(label_sequence))
+
                 for column in input_sequence:
                     if input_sequence[column].isnull().values.any():
                         input_timesteps_sequence.append(OOV_TOKEN)
@@ -75,3 +80,64 @@ class WindowGenerator:
                 all_input_timestep_sequences.append(input_timesteps_sequence)
 
         return all_input_timestep_sequences, all_labels
+
+    def split_single_input_sequence(self, data: pandas.DataFrame) -> Tuple[np.ndarray, np.ndarray]:
+        """
+        Creates a single input sequence for prediction
+        Only the clincial text of the last visit forms input
+
+        Output - Labels:  Binary classification of output width (0 no revisit, 1 revisit)
+               - Input Sequences: Textual input sequences
+        Args:
+            data (pandas.DataFrame): Dataframe with reorganised data.
+            save_windows (bool, optional): Save windows variables to file. Defaults to True.
+
+        Returns:
+            Tuple[np.ndarray, np.ndarray]: Input Sequences, Labels
+        """
+        
+        data = data.head(100)
+        non_null_indexes = list(
+            zip(*np.where(data.notnull()))
+        )  # Get indexes of df where values which are not null
+
+        all_labels: list = []
+        input_timesteps_sequence: list = []
+
+        for i in tqdm.tqdm(range(len(data.index)), ncols=100, desc="Windowing data..."):
+            visit_indexes = [
+                item[1] for item in non_null_indexes if item[0] == i
+            ]  # Indexes of all EHR entries for patient
+            for visit_index in visit_indexes:
+
+                upper_bound = clamp(
+                    self.minimum_day_of_year,
+                    visit_index + self.output_width,
+                    self.maximum_day_of_year,
+                )
+
+                input_timesteps_sequence.append(data.iat[i, visit_index])
+                label_sequence = data.iloc[i:, visit_index + 1 : upper_bound]
+                all_labels.append(self._categorize_output_sequence(label_sequence))
+
+        if self.save_windows:
+            print("------Saving windows for reuse ------")
+            with open(REARRANGED_SINGLE_INPUT_WINDOWED_DATA_FILEPATH, "wb") as f:
+                pickle.dump(input_timesteps_sequence, f)
+            with open(REARRANGED_SINGLE_INPUT_WINDOWED_LABEL_FILEPATH, "wb") as f:
+                pickle.dump(all_labels, f)
+        return input_timesteps_sequence, all_labels
+
+    def _categorize_output_sequence(self, output_sequence: pandas.DataFrame) -> bool:
+        """Categorise output sequence to binary
+        Classification is based on if output sequence is not null in the output width
+        Args:
+            output_sequence (pandas.DataFrame): Sequence to classify
+
+        Returns:
+            bool: 0 = no revisit, 1 = revisit
+        """
+        if output_sequence.isnull().all().all():
+            return 0
+        else:
+            return 1
