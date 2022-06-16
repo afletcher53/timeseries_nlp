8521b5c99f377ce9f1ad6eb7c23585e104e7f986
afletcher53 <afletcher53@gmail.com>
2022-06-16 23:24:21 +0100

    created multi timestep sequenced input

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from cmath import sin
 import pickle
 import warnings
 from os.path import exists
@@ -15,11 +16,14 @@ from constants import (
     BATCH_SIZE,
     DATA_FILEPATH,
     EMBEDDING_DIM,
+    EMPTY_TIMESTEP_TOKEN,
     GLOVE_300D_FILEPATH,
     MAX_SEQUENCE_LENGTH,
     MAX_VOCAB_SIZE,
     NUM_EPOCHS,
     REARRANGED_DATA_FILEPATH,
+    REARRANGED_MULTI_INPUT_WINDOWED_DATA_FILEPATH,
+    REARRANGED_MULTI_INPUT_WINDOWED_LABEL_FILEPATH,
     REARRANGED_SINGLE_INPUT_WINDOWED_DATA_FILEPATH,
     REARRANGED_SINGLE_INPUT_WINDOWED_LABEL_FILEPATH,
     TEST_TRAIN_SPLIT,
@@ -30,126 +34,7 @@ tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir="logs")
 AUTOTUNE = tf.data.AUTOTUNE
 
 
-def main():
-
-    """
-    1 - Load datafile
-    2 - Rearrange datafile if needed, if present skip
-    3 - Create TextVectorization
-    4 - Window Data
-    5 - Section into test/train/split
-    6 - Embed vectors
-    7 - Fit and Train Model
-    """
-
-    """
-    1. Load Datafile
-    """
-
-    if not exists(DATA_FILEPATH):
-        raise ValueError("No datafile supplied.")
-
-    for _ in tqdm(range(0, 100), ncols=100, desc="Loading data.."):
-        df = pd.read_csv(DATA_FILEPATH, delimiter="\t", encoding="latin-1")
-    print(f"------Loading {DATA_FILEPATH} is completed ------")
-
-    doy = []  # Calc the day of the year for each entry in file
-    for index in range(len(df)):
-        d1 = datetime.strptime(df.iloc[index].date, "%Y-%m-%d %H:%M:%S")
-        day_of_year = d1.timetuple().tm_yday  # returns 1 for January 1st
-        doy.append(day_of_year)
-    df["day_of_year"] = doy
-
-    print(f"Total EHRs: {len(df.index)}")
-    print(f"Average EHR character length: {df.ehr.apply(len).mean()}")
-
-    """
-    2. Create rearranged datafile, if needed
-    """
-
-    # New dataframe to hold changed data shape - want to have columns equal to every day of the year, with each row indicating a specific patient. EHR entries are located in each cell
-    if not exists(REARRANGED_DATA_FILEPATH):
-        doy = list(range(0, 365))  # Unsuprisingly, there are 365 days in a year
-        ts_df = pd.DataFrame(
-            columns=doy
-        )  # add 365 day of year columns to the new dataframe
-        max_patient_num: int = len(
-            df.index
-        )  # Assumption is that this is Z set i.e. {0, ..., 365}
-        for i in tqdm(range(max_patient_num), desc="Rearranging patient data"):
-            rows = df.loc[df.patient_id == i]
-            for index, row in rows.iterrows():
-                ts_df.at[i, row.day_of_year] = row.ehr
-        print("------ Patient data restructuring is completed ------")
-        ts_df.to_csv(REARRANGED_DATA_FILEPATH, index=False)
-
-    time_series_df = pd.read_csv(REARRANGED_DATA_FILEPATH)
-
-    """
-    3. Create TextVectorization object
-    """
-
-    X_train_text = df.ehr
-    text_vectorization: TextVectorization = TextVectorization(
-        output_mode="int",
-        split="whitespace",
-        max_tokens=MAX_VOCAB_SIZE,
-        output_sequence_length=MAX_SEQUENCE_LENGTH,
-    )
-    text_vectorization.adapt(X_train_text)
-
-    """
-    4. Window data with WindowGenerator
-    """
-
-    if not exists(REARRANGED_SINGLE_INPUT_WINDOWED_LABEL_FILEPATH) and not exists(
-        REARRANGED_SINGLE_INPUT_WINDOWED_LABEL_FILEPATH
-    ):
-        w1 = WindowGenerator(input_width=30, output_width=30, save_windows=True)
-        w1.window_single_input_sequence(time_series_df)
-    with open(REARRANGED_SINGLE_INPUT_WINDOWED_DATA_FILEPATH, "rb") as f:
-        loaded_dataset = pickle.load(f)
-
-    with open(REARRANGED_SINGLE_INPUT_WINDOWED_LABEL_FILEPATH, "rb") as f:
-        loaded_labels = pickle.load(f)
-
-    print("------ Windowed Data Loaded ------")
-
-    """
-    5. Section Data into test/train/split
-    """
-
-    dataset = list(
-        map(
-            text_vectorization, tqdm(loaded_dataset, ncols=100, desc="Vectorizing data")
-        )
-    )  # Vectorize the data with the TextVect object
-
-    dataset = numpy.array(dataset)
-    loaded_labels = numpy.array(loaded_labels)
-
-    X_train_val, X_test, Y_train_val, Y_test = train_test_split(
-        dataset, loaded_labels, test_size=TEST_TRAIN_SPLIT
-    )
-
-    # Split the remaining data to train and validation
-    X_train, X_val, Y_train, Y_val = train_test_split(
-        X_train_val, Y_train_val, test_size=TEST_TRAIN_SPLIT, shuffle=True
-    )
-
-    print(
-        X_train.shape,
-        X_val.shape,
-        X_test.shape,
-        Y_train.shape,
-        Y_val.shape,
-        Y_test.shape,
-    )  # (num_samples, max_sequence_length)
-
-    """
-    6. Embed vectors
-    """
-
+def embed_vectors(text_vectorization):
     embeddings_index = {}
 
     f = open(GLOVE_300D_FILEPATH, encoding="UTF-8")
@@ -170,10 +55,131 @@ def main():
         embedding_vector = embeddings_index.get(word)
         if embedding_vector is not None:
             embedding_matrix[i] = embedding_vector
+    return embedding_matrix
 
-    """
-    7. Fit and train models
-    # """
+
+def test_train_splt(loaded_labels, dataset):
+    X_train_val, X_test, Y_train_val, Y_test = train_test_split(
+        dataset, loaded_labels, test_size=TEST_TRAIN_SPLIT
+    )
+
+    # Split the remaining data to train and validation
+    X_train, X_val, Y_train, Y_val = train_test_split(
+        X_train_val, Y_train_val, test_size=TEST_TRAIN_SPLIT, shuffle=True
+    )
+
+    print(
+        X_train.shape,
+        X_val.shape,
+        X_test.shape,
+        Y_train.shape,
+        Y_val.shape,
+        Y_test.shape,
+    )
+    # (num_samples, max_sequence_length)
+    return X_test, Y_test, X_train, Y_train
+
+
+def vectorize_data_single_timestep(text_vectorization, loaded_dataset):
+    dataset = list(
+        map(
+            text_vectorization, tqdm(loaded_dataset, ncols=100, desc="Vectorizing data")
+        )
+    )
+    return dataset
+
+
+def generate_windows(time_series_df, multi: bool = False):
+    if multi:
+        if not exists(REARRANGED_MULTI_INPUT_WINDOWED_DATA_FILEPATH) and not exists(
+            REARRANGED_SINGLE_INPUT_WINDOWED_LABEL_FILEPATH
+        ):
+            w1 = WindowGenerator(input_width=30, output_width=30, save_windows=True)
+            w1.window_multi_input_sequence(time_series_df)
+        with open(REARRANGED_MULTI_INPUT_WINDOWED_DATA_FILEPATH, "rb") as f:
+            loaded_dataset = pickle.load(f)
+
+        with open(REARRANGED_MULTI_INPUT_WINDOWED_LABEL_FILEPATH, "rb") as f:
+            loaded_labels = pickle.load(f)
+    else:
+        if not exists(REARRANGED_SINGLE_INPUT_WINDOWED_DATA_FILEPATH) and not exists(
+            REARRANGED_SINGLE_INPUT_WINDOWED_LABEL_FILEPATH
+        ):
+            w1 = WindowGenerator(input_width=30, output_width=30, save_windows=True)
+            w1.window_single_input_sequence(time_series_df)
+        with open(REARRANGED_SINGLE_INPUT_WINDOWED_DATA_FILEPATH, "rb") as f:
+            loaded_dataset = pickle.load(f)
+
+        with open(REARRANGED_SINGLE_INPUT_WINDOWED_LABEL_FILEPATH, "rb") as f:
+            loaded_labels = pickle.load(f)
+
+    print("------ Windowed Data Loaded ------")
+    return loaded_dataset, loaded_labels
+
+
+def create_textvectorisation(df):
+    X_train_text = df.ehr
+    text_vectorization: TextVectorization = TextVectorization(
+        output_mode="int",
+        split="whitespace",
+        max_tokens=MAX_VOCAB_SIZE,
+        output_sequence_length=MAX_SEQUENCE_LENGTH,
+    )
+    text_vectorization.adapt(X_train_text)
+    return text_vectorization
+
+
+def refactor_dataframe(df):
+    # New dataframe to hold changed data shape - want to have columns equal to every day of the year, with each row indicating a specific patient. EHR entries are located in each cell
+    if not exists(REARRANGED_DATA_FILEPATH):
+        doy = list(range(0, 365))  # Unsuprisingly, there are 365 days in a year
+        ts_df = pd.DataFrame(
+            columns=doy
+        )  # add 365 day of year columns to the new dataframe
+        max_patient_num: int = len(
+            df.index
+        )  # Assumption is that this is Z set i.e. {0, ..., 365}
+        for i in tqdm(range(max_patient_num), desc="Rearranging patient data"):
+            rows = df.loc[df.patient_id == i]
+            for index, row in rows.iterrows():
+                ts_df.at[i, row.day_of_year] = row.ehr
+        print("------ Patient data restructuring is completed ------")
+        ts_df.to_csv(REARRANGED_DATA_FILEPATH, index=False)
+
+    time_series_df = pd.read_csv(REARRANGED_DATA_FILEPATH)
+    return time_series_df
+
+
+def load_datafile():
+    if not exists(DATA_FILEPATH):
+        raise ValueError("No datafile supplied.")
+
+    for _ in tqdm(range(0, 100), ncols=100, desc="Loading data.."):
+        df = pd.read_csv(DATA_FILEPATH, delimiter="\t", encoding="latin-1")
+    print(f"------Loading {DATA_FILEPATH} is completed ------")
+
+    doy = []  # Calc the day of the year for each entry in file
+    for index in range(len(df)):
+        d1 = datetime.strptime(df.iloc[index].date, "%Y-%m-%d %H:%M:%S")
+        day_of_year = d1.timetuple().tm_yday  # returns 1 for January 1st
+        doy.append(day_of_year)
+    df["day_of_year"] = doy
+
+    print(f"Total EHRs: {len(df.index)}")
+    print(f"Average EHR character length: {df.ehr.apply(len).mean()}")
+    return df
+
+
+def single_timestep_predictions():
+    df = load_datafile()
+    time_series_df = refactor_dataframe(df)
+    text_vectorization = create_textvectorisation(df)
+    loaded_dataset, loaded_labels = generate_windows(time_series_df)
+    dataset = vectorize_data_single_timestep(text_vectorization, loaded_dataset)
+    dataset = numpy.array(dataset)
+    loaded_labels = numpy.array(loaded_labels)
+    X_test, Y_test, X_train, Y_train = test_train_splt(loaded_labels, dataset)
+    embedding_matrix = embed_vectors(text_vectorization)
 
     embedding_layer = Embedding(
         MAX_VOCAB_SIZE,
@@ -204,6 +210,35 @@ def main():
     )
 
     print(LSTM1.model.evaluate(X_test, Y_test))
+
+
+def multiple_timestep_prediction():
+    df = load_datafile()
+    time_series_df = refactor_dataframe(df)
+    text_vectorization = create_textvectorisation(df)
+    loaded_dataset, loaded_labels = generate_windows(time_series_df, multi=True)
+    test = vectorize_data_multi_timestep(text_vectorization, loaded_dataset)
+    print(test.shape)
+
+
+def vectorize_data_multi_timestep(text_vectorization, loaded_dataset):
+    arr = numpy.array(loaded_dataset)
+    arr[pd.isnull(arr)] = EMPTY_TIMESTEP_TOKEN
+    input_samples = []
+    for _, item in enumerate(
+        tqdm(arr, desc="Vectoring multi timestep"),
+    ):
+        time_seq = []
+        for _, timestep in enumerate(item):
+            time_seq.append(text_vectorization(timestep))
+        input_samples.append(time_seq)
+    test = numpy.array(input_samples)
+    return test
+
+
+def main():
+    multiple_timestep_prediction()
+    # single_timestep_predictions()
 
 
 if __name__ == "__main__":
