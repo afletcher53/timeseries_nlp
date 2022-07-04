d9e1103976ed964d1f97f76af464d100757f0e69
afletcher53 <afletcher53@gmail.com>
2022-07-04 22:01:57 +0100

    added initial conv1d time distributed

--- constants.py
+++ constants.py
@@ -9,21 +9,23 @@ NUM_EPOCHS: int = 20
 TEST_TRAIN_SPLIT: float = 0.2
 VALIDATION_SPLIT: float = 0.2
 
-SEED = 2022
-DATA_FILEPATH = "./data/data.csv"
-REARRANGED_DATA_FILEPATH = "./data/rearranged_data.csv"
-REARRANGED_SINGLE_INPUT_WINDOWED_DATA_FILEPATH = "./data/windowed_single_input.pkl"
-REARRANGED_SINGLE_INPUT_WINDOWED_LABEL_FILEPATH = "./data/windowed_single_labels.pkl"
-REARRANGED_MULTI_INPUT_WINDOWED_DATA_FILEPATH = "./data/windowed_MULTI_input.pkl"
-REARRANGED_MULTI_INPUT_WINDOWED_LABEL_FILEPATH = "./data/windowed_MULTI_labels.pkl"
+SEED: int = 2022
+DATA_FILEPATH: str = "./data/data.csv"
+REARRANGED_DATA_FILEPATH: str = "./data/rearranged_data.csv"
+REARRANGED_SINGLE_INPUT_WINDOWED_DATA_FILEPATH: str = "./data/windowed_single_input.pkl"
+REARRANGED_SINGLE_INPUT_WINDOWED_LABEL_FILEPATH: str = (
+    "./data/windowed_single_labels.pkl"
+)
+REARRANGED_MULTI_INPUT_WINDOWED_DATA_FILEPATH: str = "./data/windowed_MULTI_input.pkl"
+REARRANGED_MULTI_INPUT_WINDOWED_LABEL_FILEPATH: str = "./data/windowed_MULTI_labels.pkl"
 MAX_SEQUENCE_LENGTH: int = 200  # max number of words in a comment to use
 MAX_VOCAB_SIZE: int = (
     10000  # how many unique words to use (i.e num rows in embedding vector)
 )
-LR = 1e-3
+LR: float = 1e-3
 
-GLOVE_300D_FILEPATH = "./data/glove.6B.300d.txt"
-EMPTY_TIMESTEP_TOKEN = "<EMPTY>"
+GLOVE_300D_FILEPATH: str = "./data/glove.6B.300d.txt"
+EMPTY_TIMESTEP_TOKEN: str = "<EMPTY>"
 TIME_STEP: int = 30
 
 X_TRAIN_MULTI_INPUT_SAVE_FILE: str = "./data/X_train_MULTI.pkl"
