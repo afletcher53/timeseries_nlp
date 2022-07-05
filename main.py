1dfbcd0686eb5b12f29bddd3db30a98e2159bf48
afletcher53 <afletcher53@gmail.com>
2022-07-05 19:29:35 +0100

    @

--- main.py
+++ main.py
@@ -101,14 +101,23 @@ def multiple_timestep_prediction(
         trainable=False,
     )
 
-    cnn_model = CNN_mdl(embedding_layer=embedding_layer)
-    cnn_model.fit(x_train, y_train, epochs=5)
-    cnn_model.evaluate(x_test=x_test, y_test=y_test)
-    stacked_lstm_mdl = LSTM_mdl(
-        submodel=LSTMSubModels.STACKED_LSTM, embedding_layer=embedding_layer
+    lstm_mdl = LSTM_mdl(
+        submodel=LSTMSubModels.LSTM,
+        embedding_layer=embedding_layer,
+        learning_rate=1e-13,
     )
-    stacked_lstm_mdl.fit(x_train, y_train, epochs=5)
-    stacked_lstm_mdl.evaluate(x_test=x_test, y_test=y_test)
+    lstm_mdl.summary()
+    lstm_mdl.fit(x_train, y_train, epochs=5)
+    lstm_mdl.evaluate(x_test=x_test, y_test=y_test)
+
+    # cnn_model = CNN_mdl(embedding_layer=embedding_layer)
+    # cnn_model.fit(x_train, y_train, epochs=5)
+    # cnn_model.evaluate(x_test=x_test, y_test=y_test)
+    # stacked_lstm_mdl = LSTM_mdl(
+    #     submodel=LSTMSubModels.STACKED_LSTM, embedding_layer=embedding_layer
+    # )
+    # stacked_lstm_mdl.fit(x_train, y_train, epochs=5)
+    # stacked_lstm_mdl.evaluate(x_test=x_test, y_test=y_test)
 
 
 def vectorize_data_multi_timestep(text_vectorization, loaded_dataset):
@@ -127,7 +136,7 @@ def vectorize_data_multi_timestep(text_vectorization, loaded_dataset):
 
 
 def main():
-    multiple_timestep_prediction(load_from_save=False)
+    multiple_timestep_prediction(load_from_save=True)
 
 
 if __name__ == "__main__":
